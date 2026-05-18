8f04720afced913eabd09f2b60db6a1259e7ae0e
greenhead <shren0812@gmail.com>
2026-05-18 23:43:28 +0900

    chore(caddy): update FOD hash after flake input update
    miniPC NixOS에서 fix-fod-hashes.sh 자동 검증 (다중 호스트 정합성).

--- modules/nixos/programs/caddy.nix
+++ modules/nixos/programs/caddy.nix
@@ -17,7 +17,7 @@ let
 
   caddyWithPlugins = pkgs.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-    hash = "sha256-VBmICI1wklu02jmgDRmmlfNc9ftK7a74uF280xzx8uc=";
+    hash = "sha256-dNmccsad8nFm2X9w/6mdgAdhPSquNm6oVhf3sNc46SQ=";
   };
 
   envFilePath = "/run/caddy/env";
